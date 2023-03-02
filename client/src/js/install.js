cf2eef0102c7c8d118b10ad24fe18705ad9e8a15
clayandemar <clayandemar@gmail.com>
2023-03-01 18:55:59 -0600

    webpack and install
    I thought there was an issue with CSS, turns out it was webpack config

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -19,12 +19,8 @@ butInstall.addEventListener('click', async () => {
   }
 
   promptEvent.prompt();
-
-  // TODO: Notice line 9 above. We want the same line here, but the value should be set to null now.
-  // Insert line here
-
-  // TODO: Notice line 10 above. We want the same line here, but the hidden value should be set to true.
-  // Insert line here
+  window.deferredPrompt = null;
+  butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
